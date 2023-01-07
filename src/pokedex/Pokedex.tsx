72b92c921592bde7998c178e1d50d458f7ba6aad
Kauê Guerra <kaueguerra@gmail.com>
2023-01-07 11:37:47 -0300

    started styling the pokemon list, installing MaterialUI and starting the pokemon display cards.

--- src/pokedex/Pokedex.tsx
+++ src/pokedex/Pokedex.tsx
@@ -1,8 +1,21 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { PokemonDetail } from '../pokemon/interfaces/PokemonDetail';
 import { getPokemonsDetails } from '../pokemon/services/getPokemonsDetails';
 import { listPokemons, PokemonListInterface } from '../pokemon/services/listPokemons';
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import { Grid } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import { CardActionArea } from '@mui/material';
 
 interface PokedexProps {
 
@@ -27,13 +40,50 @@ export const Pokedex: React.FC<PokedexProps> = () => {
 
   return(
     <div>
-      <h1>Pokedex</h1>
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Pokedex
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
 
-      Pokemons:
-      {pokemons.map((pokemon)=> <button onClick={()=> setselectedPokemon(pokemon)}>{pokemon.name}</button>)}
+    <Container maxWidth="lg">
+      <Box mt={2}>
+      <Grid container spacing={2} >
+        {pokemons.map((pokemon)=> (
+        <Grid item xs={6} lg={3}>
+          <Card variant="outlined">
+            <CardActionArea>
+              <CardContent onClick={()=> setselectedPokemon(pokemon)} >
+                <Typography variant="h5" component="h2">
+                  {pokemon.name}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        ))}
+      </Grid>
 
-      <h2>Pokemon Selecionado: {selectedPokemon?.name || "Nenhum pokemon selecionado."}</h2>
-      {JSON.stringify(selectedPokemonDetails, undefined, 2)}
+        <h2>Pokemon Selecionado: {selectedPokemon?.name || "Nenhum pokemon selecionado."}</h2>
+        {JSON.stringify(selectedPokemonDetails, undefined, 2)}
+      </Box>
+    </Container> 
+
+
+     
     </div>
   )
 } 
